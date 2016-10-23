cc68a86e7d25b928fee51924fe75b35307f6633a
Jiří Fatka <ntsfka@gmail.com>
2016-10-23 22:53:53 +0200

    Improved tokenizer app output for unicode charaters.

--- tokenizer/main.cpp
+++ tokenizer/main.cpp
@@ -36,9 +36,27 @@ using namespace shard;
 
 /* ************************************************************************* */
 
+namespace {
+
+/* ************************************************************************* */
+
 struct ValueHelper
 {
     const tokenizer::Token& token;
+};
+
+/* ************************************************************************* */
+
+struct CharHelper
+{
+    const tokenizer::Token::CharType& ch;
+};
+
+/* ************************************************************************* */
+
+struct StringHelper
+{
+    const String& str;
 };
 
 /* ************************************************************************* */
@@ -60,6 +78,118 @@ std::ostream& operator<<(std::ostream& os, tokenizer::TokenType type) noexcept
     return os;
 }
 
+/* ************************************************************************ */
+
+/// Convert table between leading zeroes to number of UTF-8 bytes.
+constexpr int __utf32_clz_to_len[32] = {
+    7,
+    6, 6, 6, 6, 6,
+    5, 5, 5, 5, 5,
+    4, 4, 4, 4, 4,
+    3, 3, 3, 3, 3,
+    2, 2, 2, 2,
+    1, 1, 1, 1, 1, 1, 1
+};
+
+/* ************************************************************************ */
+
+/// Initial UTF-8 mark depending on sequence length.
+constexpr uint8_t __utf8_mark[7] = {
+    0x00, 0x00, 0xc0, 0xe0, 0xf0, 0xf8, 0xfc
+};
+
+/* ************************************************************************ */
+
+/**
+ * @brief Obtain required number of UTF-8 bytes.
+ * @param c UNICODE codepoint.
+ * @return Required number of bytes.
+ */
+inline int utf8clen(uint32_t c) noexcept
+{
+    // Use number of leading zeroes as index to table.
+    return __utf32_clz_to_len[__builtin_clz(c | 1)];
+}
+
+/* ************************************************************************* */
+
+/**
+ * @brief Write token character.
+ */
+std::ostream& operator<<(std::ostream& os, const CharHelper& chr) noexcept
+{
+    char buf[8];
+    uint32_t cp = chr.ch;
+
+    // Escape sequences
+    switch (cp)
+    {
+    case '\0': return os << "\"\\0\"";
+    case '\a': return os << "\"\\a\"";
+    case '\b': return os << "\"\\b\"";
+    case '\t': return os << "\"\\t\"";
+    case '\n': return os << "\"\\n\"";
+    case '\v': return os << "\"\\v\"";
+    case '\f': return os << "\"\\f\"";
+    case '\r': return os << "\"\\r\"";
+    case '\e': return os << "\"\\e\"";
+    case '\\': return os << "\"\\\\\"";
+    case '"':  return os << "\"\\\"\"";
+    }
+
+    // Non-printable characters
+    if (cp < ' ')
+        return os << static_cast<int>(cp);
+
+    // Number of required bytes
+    const int count = utf8clen(cp);
+
+    switch (count)
+    {
+      case 7: return os;
+      case 6: buf[5] = (cp | 0x80) & 0xbf; cp >>= 6;
+      case 5: buf[4] = (cp | 0x80) & 0xbf; cp >>= 6;
+      case 4: buf[3] = (cp | 0x80) & 0xbf; cp >>= 6;
+      case 3: buf[2] = (cp | 0x80) & 0xbf; cp >>= 6;
+      case 2: buf[1] = (cp | 0x80) & 0xbf; cp >>= 6;
+      case 1: buf[0] = (cp | __utf8_mark[count]);
+    }
+
+    return (os << '\"').write(buf, count) << '\"';
+}
+
+/* ************************************************************************* */
+
+/**
+ * @brief Write token string.
+ */
+std::ostream& operator<<(std::ostream& os, const StringHelper& str) noexcept
+{
+    os << '\"';
+
+    for (auto c : str.str)
+    {
+        // Escape sequences
+        switch (c)
+        {
+        case '\0': os << "\\0"; break;
+        case '\a': os << "\\a"; break;
+        case '\b': os << "\\b"; break;
+        case '\t': os << "\\t"; break;
+        case '\n': os << "\\n"; break;
+        case '\v': os << "\\v"; break;
+        case '\f': os << "\\f"; break;
+        case '\r': os << "\\r"; break;
+        case '\e': os << "\\e"; break;
+        case '\\': os << "\\\\"; break;
+        case '"':  os << "\\\""; break;
+        default:   os << c; break;
+        }
+    }
+
+    return os << '\"';
+}
+
 /* ************************************************************************* */
 
 /**
@@ -75,10 +205,10 @@ std::ostream& operator<<(std::ostream& os, const ValueHelper& value) noexcept
     switch (token.getType())
     {
     case tokenizer::TokenType::Identifier:
-    case tokenizer::TokenType::String:  return os << "\"" << token.getStringValue() << "\"";
+    case tokenizer::TokenType::String:  return os << StringHelper{token.getStringValue()};
     case tokenizer::TokenType::Float:   return os << token.getFloatValue();
-    case tokenizer::TokenType::Char:    return os << token.getCharValue();
     case tokenizer::TokenType::Int:     return os << token.getIntValue();
+    case tokenizer::TokenType::Char:    return os << CharHelper{token.getCharValue()};
 
     case tokenizer::TokenType::Keyword:
     {
@@ -121,6 +251,10 @@ tokenizer::Tokenizer createTokenizer(StringView filename)
     );
 
     return tokenizer::Tokenizer(moveValue(input));
+}
+
+/* ************************************************************************* */
+
 }
 
 /* ************************************************************************* */
